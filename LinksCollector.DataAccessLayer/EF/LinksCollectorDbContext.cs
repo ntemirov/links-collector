0d8d996c1ea537f1a40d1d084dedf38314c2c70d
Назир Темиров <rizan09@outlook.com>
2017-08-03 00:39:05 +0300

    Database context was added

---
+++ LinksCollector.DataAccessLayer/EF/LinksCollectorDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using LinksCollector.DataAccessLayer.Configurations;
+
+namespace LinksCollector.DataAccessLayer.EF
+{
+    public class LinksCollectorDbContext : DbContext
+    {
+        public LinksCollectorDbContext() : base() { }
+
+        public LinksCollectorDbContext(DbContextOptions<LinksCollectorDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.AddConfiguration(new RequestDataModelMap());
+        }
+    }
+}
