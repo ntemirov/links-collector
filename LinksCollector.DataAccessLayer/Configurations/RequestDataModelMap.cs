c96ea3fe5d548d41a70ee7380dfdcf70042d09f1
Назир Темиров <rizan09@outlook.com>
2017-08-03 00:53:49 +0300

    Data models configurations was added

---
+++ LinksCollector.DataAccessLayer/Configurations/RequestDataModelMap.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using LinksCollector.DataAccessLayer.DataModels;
+
+namespace LinksCollector.DataAccessLayer.Configurations
+{
+    public class RequestDataModelMap : EntityTypeConfiguration<RequestDataModel>
+    {
+        public override void Map(EntityTypeBuilder<RequestDataModel> builder)
+        {
+            builder.ToTable("Requests");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Url)
+                .HasMaxLength(500)
+                .IsRequired();
+
+            builder.Property(p => p.CreatedAt)
+                .IsRequired()
+                .HasDefaultValueSql("GetUtcDate()");
+            builder.Property(p => p.CreatedAt);
+
+            builder.Property(p => p.HyperlinksCount)
+                .IsRequired();
+        }
+    }
+}
