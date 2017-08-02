ff02937cbe5ffc71f93d8880e07b3f48a2e2bcfb
Назир Темиров <rizan09@outlook.com>
2017-08-03 00:53:18 +0300

    Unit of work was added

---
+++ LinksCollector.DataAccessLayer/Uow/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LinksCollector.DataAccessLayer.Uow
+{
+    /// <summary>
+    /// Unit of work.
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        /// <summary>
+        /// Commits current changes in the unit of work
+        /// </summary>
+        Task<Int32> Commit();
+    }
+}
