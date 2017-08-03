c974319080e67c19c44726b28f183a54f1b309a3
Назир Темиров <rizan09@outlook.com>
2017-08-03 21:16:04 +0300

    Dependency injection for repositories, services and db context was added

--- LinksCollector.Web/Startup.cs
+++ LinksCollector.Web/Startup.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+using LinksCollector.DataAccessLayer.EF;
 
 namespace LinksCollector_Web
 {
@@ -22,6 +25,10 @@ namespace LinksCollector_Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add framework services.
+            services.AddDbContext<LinksCollectorDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddMvc();
         }
 
