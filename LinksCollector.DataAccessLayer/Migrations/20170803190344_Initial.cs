ecdfadec37fd5350f9b947feffa33813c28d5cb9
Назир Темиров <rizan09@outlook.com>
2017-08-03 22:05:56 +0300

    Migrations

---
+++ LinksCollector.DataAccessLayer/Migrations/20170803190344_Initial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace LinksCollector.DataAccessLayer.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Requests",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false, defaultValueSql: "GetUtcDate()"),
+                    HyperlinksCount = table.Column<int>(type: "int", nullable: false),
+                    RequestDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    RequestProcessingTime = table.Column<TimeSpan>(type: "time", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    Url = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requests", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Requests");
+        }
+    }
+}
